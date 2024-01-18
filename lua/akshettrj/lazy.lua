6b16d4dc38e872f2209fe735ead486d48274a87f
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2024-01-18 16:35:13 +0530

    fix lazy installation on ada

--- lua/akshettrj/lazy.lua
+++ lua/akshettrj/lazy.lua
@@ -1,5 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
+if (vim.uv ~= nil) and (not vim.uv.fs_stat(lazypath)) then
   vim.fn.system({
     "git",
     "clone",
