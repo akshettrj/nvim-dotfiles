1bcaeee644f43876dae0c0f9d52f2d6e4a32aa44
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2024-02-02 21:55:45 +0530

    fix copilot

--- lua/akshettrj/plugins/copilot.lua
+++ lua/akshettrj/plugins/copilot.lua
@@ -1,15 +1,20 @@
 return {
   {
     "https://github.com/github/copilot.vim",
-    cmd = { "Copilot" },
+    event = { "VeryLazy" },
     init = function()
       vim.g.copilot_filetypes = {
         ["*"] = false,
-        lua = true,
       }
-      vim.g.copilot_enabled = false
+      vim.g.copilot_enabled = true
       vim.g.copilot_no_tab_map = true
       vim.g.copilot_no_maps = true
+    end,
+    config = function()
+      vim.api.nvim_set_keymap("i", "<A-c>", "copilot#Suggest()", { expr = true, desc = "[Copilot] Get suggestions" })
+      vim.api.nvim_set_keymap("i", "<A-]>", "copilot#Next()", { expr = true, desc = "[Copilot] Next suggestion" })
+      vim.api.nvim_set_keymap("i", "<A-[>", "copilot#Previous()", { expr = true, desc = "[Copilot] Previous suggestion" })
+      vim.api.nvim_set_keymap("i", "<A-\\>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false, desc = "[Copilot] Accept suggestion" })
     end,
   },
 }
