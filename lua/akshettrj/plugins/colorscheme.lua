af3d4a95d750eec728de5b05efa82411cde3ae9d
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2024-02-02 23:25:27 +0530

    transparency

--- lua/akshettrj/plugins/colorscheme.lua
+++ lua/akshettrj/plugins/colorscheme.lua
@@ -27,7 +27,7 @@ return {
       palette_overrides = {},
       overrides = {},
       dim_inactive = false,
-      transparent_mode = false,
+      transparent_mode = true,
     },
   },
 }
--- lua/akshettrj/plugins/treesitter.lua
+++ lua/akshettrj/plugins/treesitter.lua
@@ -20,6 +20,16 @@ return {
   {
     "https://github.com/nvim-treesitter/nvim-treesitter-context",
     event = { "VeryLazy" },
+    config = function()
+      require("treesitter-context").setup()
+      highlight_settings = {
+        underline = true,
+        sp = "Grey",
+        bg = "Black",
+      }
+      vim.api.nvim_set_hl(0, "TreesitterContext", highlight_settings)
+      vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", highlight_settings)
+    end,
     dependencies = {
       "https://github.com/nvim-treesitter/nvim-treesitter",
     }
