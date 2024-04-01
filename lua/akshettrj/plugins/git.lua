35fd72da70da4a707f5a02d8ad32de9cd130871f
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2024-04-01 22:55:04 +0530

    [gitsigns] add description to keybindings

--- lua/akshettrj/plugins/git.lua
+++ lua/akshettrj/plugins/git.lua
@@ -52,22 +52,24 @@ return {
         update_debounce = 100,
         max_file_length = 40000,
         on_attach = function()
-          opts = {
-            expr = true,
-            silent = true,
-          }
+          local map = function(mode, lhs, rhs, opts)
+            opts["expr"] = true
+            opts["silent"] = true
 
-          vim.keymap.set("n", "]c", function()
+            vim.keymap.set(mode, lhs, rhs, opts)
+          end
+
+          map("n", "]c", function()
             if vim.wo.diff then return "]c" end
             vim.schedule(function() gs.next_hunk() end)
             return "<Ignore>"
-          end, opts)
+          end, { desc = "Go to next git hunk" })
 
-          vim.keymap.set("n", "[c", function()
+          map("n", "[c", function()
             if vim.wo.diff then return "[c" end
             vim.schedule(function() gs.prev_hunk() end)
             return "<Ignore>"
-          end, opts)
+          end, { desc = "Go to previous git hunk" })
         end,
       }
     end,
