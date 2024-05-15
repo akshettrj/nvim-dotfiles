ed575fae36594bcbc060d89f79c66c644d263698
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2024-05-15 13:06:50 +0530

    [lazy] add lf.nvim for easy file exploring

---
+++ lua/akshettrj/plugins/lf.lua
@@ -0,0 +1,22 @@
+return {
+  "https://github.com/lmburns/lf.nvim",
+  keys = {
+    { "<A-o>", "<Cmd>Lf<CR>", silent = true, desc = "Open LF file manager" },
+  },
+  config = function ()
+    require("lf").setup({
+      escape_quit = false,
+      border = "rounded",
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LfTermEnter",
+      callback = function (a)
+        vim.api.nvim_buf_set_keymap(a.buf, "t", "q", "q", {nowait = true})
+      end
+    })
+  end,
+  dependencies = {
+    "https://github.com/akinsho/toggleterm.nvim",
+  },
+}
