3168e8e1c18a078c992a58c605ad729d9991658f
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2024-05-29 17:11:21 +0530

    [opts] move laststatus to global options

--- lua/akshettrj/opts.lua
+++ lua/akshettrj/opts.lua
@@ -18,7 +18,7 @@ vim.opt.signcolumn = "yes"
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.laststatus = 2
+vim.opt_global.laststatus = 2
 -- vim.opt.winbar = "%=%m %f"
 vim.opt.termguicolors = true
 vim.opt_global.termguicolors = true
