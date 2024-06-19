ef83af2c8de5cb63f275a7290cc35e5e42af9cee
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-06-19 23:44:21 +0530

    cloak: disable the plugin

--- lua/akshettrj/plugins/cloak.lua
+++ lua/akshettrj/plugins/cloak.lua
@@ -2,7 +2,7 @@ return {
   "https://github.com/laytan/cloak.nvim",
   lazy = false,
   opts = {
-    enabled = true,
+    enabled = false,
     cloak_character = '*',
     highlight_group = 'Comment',
     cloak_length = nil, -- Provide a number if you want to hide the true length of the value.
