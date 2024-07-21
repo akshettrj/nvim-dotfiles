fa9b152ed23575759f344a5f9c9ae67c6aeefe57
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-07-21 16:24:52 +0530

    plugins: add support for dap

---
+++ lua/akshettrj/plugins/dap.lua
@@ -0,0 +1,105 @@
+return {
+  {
+    "https://github.com/mfussenegger/nvim-dap",
+    keys = {
+      {
+        "<leader>db",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Toggle breakpoint for debugging",
+      },
+      {
+        "<leader>dB",
+        function()
+          require("dap").run_to_cursor()
+        end,
+      },
+      {
+        "<leader>?",
+        function()
+          require("dap").eval(nil, { enter = true })
+        end,
+      },
+      {
+        "<F1>",
+        function()
+          require("dap").continue()
+        end,
+      },
+      {
+        "<F2>",
+        function()
+          require("dap").step_into()
+        end,
+      },
+      {
+        "<F3>",
+        function()
+          require("dap").step_over()
+        end,
+      },
+      {
+        "<F4>",
+        function()
+          require("dap").step_out()
+        end,
+      },
+      {
+        "<F5>",
+        function()
+          require("dap").step_back()
+        end,
+      },
+      {
+        "<F6>",
+        function()
+          require("dap").restart()
+        end,
+      },
+      {
+        "<F7>",
+        function()
+          require("dap").close()
+        end,
+      },
+    },
+    config = function()
+      local dap = require("dap")
+      local ui = require("dapui")
+
+      ui.setup()
+      require("dap-go").setup()
+      require("dap-python").setup("python")
+
+      require("nvim-dap-virtual-text").setup({
+        display_callback = function(variable)
+          if #variable.value > 15 then
+            return " " .. string.sub(variable.value, 1, 15) .. "... "
+          end
+          return " " .. variable.value
+        end,
+      })
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+    dependencies = {
+      "https://github.com/leoluz/nvim-dap-go",
+      "https://github.com/mfussenegger/nvim-dap-python",
+      "https://github.com/theHamsta/nvim-dap-virtual-text",
+      "https://github.com/nvim-neotest/nvim-nio",
+      "https://github.com/rcarriga/nvim-dap-ui"
+    },
+  },
+}
