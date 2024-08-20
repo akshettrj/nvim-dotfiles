542a1a3d0f497a90dced650c629b00d64c1cce2c
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-08-20 20:53:26 +0530

    lazy: install render-markdown

---
+++ lua/akshettrj/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "https://github.com/MeanderingProgrammer/markdown.nvim",
+    main = "render-markdown",
+    name = "render-markdown",
+    opts = {},
+    dependencies = {
+      "https://github.com/nvim-treesitter/nvim-treesitter",
+      "https://github.com/echasnovski/mini.nvim",
+    },
+  },
+}
