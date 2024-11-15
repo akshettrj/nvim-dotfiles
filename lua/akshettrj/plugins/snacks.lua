050411ce215635159c365de64ce73667a5c8b96e
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-11-15 21:15:04 +0530

    feat: install snacks.nvim plugin

---
+++ lua/akshettrj/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return   {
+  "https://github.com/folke/snacks.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    bigfile = { enable = true },
+    gitbrowse = { enable = true },
+  },
+}
+
