105f1526785138ca46697423665ca99ee51b2fbb
Akshett Rai Jindal <akshettrai.jindal@sprinklr.com>
2025-03-15 14:56:18 +0530

    feat(telescope): ignore .venv directories

--- lua/akshettrj/plugins/telescope.lua
+++ lua/akshettrj/plugins/telescope.lua
@@ -41,7 +41,7 @@ return {
         defaults = {
           file_ignore_patterns = {
             "node_modules/.*",
-            "venv/.*",
+            "%.?venv/.*",
             ".git/.*"
           },
           mappings = {
