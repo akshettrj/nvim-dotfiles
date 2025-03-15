1d271405fcf7e5de6c70bef5d0e251a7ee7fbfd2
Akshett Rai Jindal <akshettrai.jindal@sprinklr.com>
2025-03-15 16:23:30 +0530

    feat(latex): use mupdf view mode for macos

--- lua/akshettrj/plugins/latex.lua
+++ lua/akshettrj/plugins/latex.lua
@@ -2,6 +2,11 @@ return {
   "https://github.com/lervag/vimtex",
   ft = { "tex" },
   init = function()
-    vim.g.vimtex_view_method = "zathura"
+    local hostname = vim.fn.hostname()
+    if hostname:startswith("akshettrai.jindal") then
+      vim.g.vimtex_view_method = "mupdf"
+    else
+      vim.g.vimtex_view_method = "zathura"
+    end
   end
 }
